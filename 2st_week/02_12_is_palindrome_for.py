d2fce5715a2ff0bc0f7004fba4f70049615c8146
seonhyeokjun <tjsrmatn@naver.com>
2025-02-02 20:19:19 +0900

    2-9. 재귀 함수 - 2

---
+++ 2st_week/02_12_is_palindrome_for.py
@@ -0,0 +1,11 @@
+input = "abcba"
+
+def is_palindrome(string):
+    n = len(string)
+    for i in range(n):
+        if string[i] != string[n - i - 1]:
+            return False
+
+    return True
+
+print(is_palindrome(input))
