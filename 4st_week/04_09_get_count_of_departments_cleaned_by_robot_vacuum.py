c35a7e59bee4e3514f6d081b2c074fc7c2abb0a0
seonhyeokjun <tjsrmatn@naver.com>
2025-02-22 20:19:19 +0900

    42. 4-9. 4주차 끝 & 숙제 설명

---
+++ 4st_week/04_09_get_count_of_departments_cleaned_by_robot_vacuum.py
@@ -0,0 +1,100 @@
+from collections import deque
+
+current_r, current_c, current_d = 7, 4, 0
+current_room_map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+# 북 동 남 서
+dr = [-1, 0, 1, 0]  # 행 변화량 (row)
+dc = [ 0, 1, 0, -1] # 열 변화량 (column)
+
+def get_count_of_departments_cleaned_by_robot_vacuum(r, c, d, room_map):
+    n = len(room_map)
+    m = len(room_map[0])
+    count_of_departments_cleaned = 1
+    room_map[r][c] = 2
+    queue = deque([(r, c, d)])
+
+    while queue:
+        r, c, d = queue.popleft()
+        temp_d = d
+
+        for i in range(4):
+            temp_d = get_d_index_when_rotate_left(temp_d)
+            new_r, new_c = r + dr[temp_d], c + dc[temp_d]
+
+            if 0 <= new_r < n and 0 <= new_c < m and room_map[new_r][new_c] == 0:
+                count_of_departments_cleaned += 1
+                room_map[new_r][new_c] = 2  # 청소한 곳은 2로 표시
+                queue.append((new_r, new_c, temp_d))
+                break
+
+            elif i == 3:  # 네 방향 모두 청소가 되어있거나 벽인 경우
+                temp_d = get_d_index_when_go_back(d)
+                new_r, new_c = r + dr[temp_d], c + dc[temp_d]
+                queue.append((new_r, new_c, d))
+
+                if room_map[new_r][new_c] == 1:  # 뒤쪽이 벽인 경우
+                    return count_of_departments_cleaned  # 청소를 종료하고 현재까지 청소한 개수를 반환
+
+def get_d_index_when_rotate_left(d):
+    return (d + 3) % 4
+
+def get_d_index_when_go_back(d):
+    return (d + 2) % 4
+
+# 57 가 출력되어야 합니다!
+print(get_count_of_departments_cleaned_by_robot_vacuum(current_r, current_c, current_d, current_room_map))
+current_room_map2 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 29 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,3,1,current_room_map2))
+current_room_map3 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 33 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(7,4,1,current_room_map3))
+current_room_map4 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 1, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+print("정답 = 25 / 현재 풀이 값 = ", get_count_of_departments_cleaned_by_robot_vacuum(6,2,0,current_room_map4))
---
+++ 4st_week/04_10_get_all_ways_of_theater_seat.py
@@ -0,0 +1,39 @@
+seat_count = 9
+vip_seat_array = [4, 7]
+memo = {
+    1: 1,
+    2: 2
+}
+
+def fibo_dynamic_programming(n, fibo_memo):
+    if n in fibo_memo:
+        return fibo_memo[n]
+
+    # n이 fibo_memo에 없다면, 재귀적으로 호출해서 값을 구합니다!
+    nth_fibo = fibo_dynamic_programming(n - 1, fibo_memo) + fibo_dynamic_programming(n - 2, fibo_memo)
+    fibo_memo[n] = nth_fibo
+
+    return nth_fibo
+
+def get_all_ways_of_theater_seat(total_count, fixed_seat_array):
+    all_ways = 1
+    current_index = 0
+
+    for fixed_seat in fixed_seat_array:
+        fixed_seat_index = fixed_seat - 1  # 1부터 시작하는 좌석 번호를 0부터 시작하는 인덱스로 변환
+        count_of_ways = fibo_dynamic_programming(fixed_seat_index - current_index, memo)
+        all_ways *= count_of_ways
+        current_index = fixed_seat_index + 1
+
+    count_of_ways = fibo_dynamic_programming(total_count - current_index, memo)
+    all_ways *= count_of_ways
+
+    return all_ways
+
+
+# 12가 출력되어야 합니다!
+print(get_all_ways_of_theater_seat(seat_count, vip_seat_array))
+
+print("정답 = 4 / 현재 풀이 값 = ", get_all_ways_of_theater_seat(9,[2,4,7]))
+print("정답 = 26 / 현재 풀이 값 = ", get_all_ways_of_theater_seat(11,[2,5]))
+print("정답 = 6 / 현재 풀이 값 = ", get_all_ways_of_theater_seat(10,[2,6,9]))
